634acade9c270b8c2a9e6590fc612c211cf5649b
Rupe88 <chyrupesh828@gmail.com>
2024-07-28 12:22:35 +0545

    front and back project setup done

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+        <div className='text-red-500'>
+      home page
+    </div>
+    <Button>Click me</Button>
+    </>
+
+  )
+}
+
+export default App
